bf63259dc28a3add95dae528e15605f9f1345bd1
Krystian Stanuch <xkriszu@gmail.com>
2019-08-06 20:33:03 +0200

    DropdownList class prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+
+type DropdownTypeState = {
+  cities: [],
+  selectedCity: string,
+  isLoading: Boolean
+}
+
+class DropdownList extends Component <{}, DropdownTypeState>{
+
+  constructor(props: String){
+    super(props)
+    this.state = {
+      cities: [],
+      selectedCity: "Not chosen",
+      isLoading: false
+    }
+  }
+
+  componentDidMount(){
+
+    this.setState({
+      isLoading: true
+    });
+
+    fetch('http://dev-weather-api.azurewebsites.net/api/city')
+      .then(res => res.json())
+        .then(data => this.setState({
+          cities: data,
+          isLoading: false  
+        }))
+  }
+
+  handleChange(e:any){
+    this.setState({
+      selectedCity: e.target.value
+    })
+  }
+
+  render(){
+
+    const {cities, selectedCity, isLoading} = this.state;
+
+    if(isLoading){
+      return <p>Loading please wait...</p>
+    }
+
+    return (
+
+      <div>
+        <select onChange={this.handleChange.bind(this)}>
+          {cities.map((city:any) => 
+              <option value={city.id}>{city.name}</option>
+              )}
+        </select>
+      </div>
+    )
+  }
+
+  
+}
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DropdownList> </DropdownList>
     </div>
   );
 }
